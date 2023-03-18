c34edab0e4996f6bf7c2b3bc36d60055dc169991
KMMRCap <mortezarezainia@gmail.com>
2023-03-18 22:31:51 +0330

    base url changed

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    BASE_URL: 'http://127.0.0.1:5000',
+    // BASE_URL: 'http://127.0.0.1:5000',
+    BASE_URL: 'http://idealmoney.io:5000',
     NEXT_SHARP_PATH: "/tmp/node_modules/sharp"
   },
   // images: {
